8995828e4b784757c1555dc91d26737fad972fe4
Jason Gao <ghpjc@hotmail.com>
2015-01-07 17:56:45 +0800

    debug

--- Extend/Library/ORG/ThinkSDK/ThinkOauth.class.php
+++ Extend/Library/ORG/ThinkSDK/ThinkOauth.class.php
@@ -160,6 +160,7 @@ abstract class ThinkOauth
                 throw new Exception('AUTHORIZE配置不正确！');
             }
         }
+        file_put_contents('/tmp/test.log', var_export($params, 1), FILE_APPEND);
         return $this->GetRequestCodeURL . '?' . http_build_query($params);
     }
 
